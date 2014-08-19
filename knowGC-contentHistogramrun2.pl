b7b0a9d20ece1536479d85ff3104490f142200e3
ramadatta <ramadatta.88@gmail.com>
2014-08-19 15:03:18 +0800

    Plot GC content across the genome moving in Sliding windows

---
+++ knowGC-contentHistogramrun2.pl
@@ -0,0 +1,42 @@
+
+$filename="$ARGV[0]";
+$min="$ARGV[1]";
+$max="$ARGV[2]";
+$int="$ARGV[3]";
+
+	for($i=$min;$i<=($max+0.01);$i+=$int)
+	{
+	#print "$i\n";
+	push(@Intervals,$i);
+	}
+
+#print "Intervals to be used for Binning: @Intervals\n";
+#print "$#Intervals\n";
+$TotalNumber=`grep -c "." $filename`;
+print "BINS\tCOUNTS\tProportion\n";
+for($k=$min;$k<=($#Intervals-1);$k++)
+{
+	$knext=$k+1;
+	$cnt=0;
+		$lowerBound=$Intervals[$k];	
+		$upperBound=$Intervals[$knext];
+		#print "Lower Bound:$lowerBound,$upperBound\n";
+	open FH,"$filename";
+	while(<FH>)
+	{
+		$num=$_;
+		if(($num>$lowerBound) && ($num<=$upperBound)) 
+		{ 
+			#print "$counting\n";
+			$cnt++;
+		}
+		
+	}
+	close(FH);
+	$proportion=($cnt/$TotalNumber);
+	print "$Intervals[$k]-$Intervals[$knext]\t";
+	printf("%f\t", $cnt);
+	printf("%f\n",$proportion);
+	$proportion=0;
+}
+		
