b727111d38aab817fbf20eaabca3bd487eabd6e5
Prakki Rama <ramadatta.88@gmail.com>
2017-11-09 13:20:29 +0800

    Update combinedAlignedfasta.pl
    added comments for code

--- run_gubbins_in_chunks/combinedAlignedfasta.pl
+++ run_gubbins_in_chunks/combinedAlignedfasta.pl
@@ -1,6 +1,6 @@
 use POSIX;
 
-@arr=`ls -1v *fastx`;
+@arr=`ls -1v *fastx`; ##chunk files in order
 print @arr;
 
 %Hash=();
@@ -8,24 +8,27 @@ foreach $tmp (@arr)
 {
 $filename="$tmp";
 open FH,"$filename";
-#$chunkSize=500000;
 
-while(<FH>)
-{
-	$header=$_;
-	$seq=<FH>;
-	chomp($header);
-	chomp($seq);
-	if(!(exists($Hash{$header})))
+	while(<FH>)
 	{
-	$Hash{$header}="$seq";
-	}
-	else
-	{
-	$Hash{$header}="$Hash{$header}"."$seq";
+		$header=$_;
+		$seq=<FH>;
+		chomp($header);
+		chomp($seq);
+
+			if(!(exists($Hash{$header})))
+			{
+			$Hash{$header}="$seq";
+			}
+			else
+			{
+			$Hash{$header}="$Hash{$header}"."$seq"; ##concatenating all the smaller chunks into bigger file
+			}
+
 	}
 }
-}
+close(FH);
+
 open FH2,">postGubbins_seqnew_ecloacae.filtered_polymorphic_sites.fasta";
 foreach $k (sort keys %Hash)
 	{
