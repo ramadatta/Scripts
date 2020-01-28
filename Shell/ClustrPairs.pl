47fa4e65bf6f69bed6579d8e417e0c27263dfd89
Prakki Rama <ramadatta.88@gmail.com>
2020-01-28 11:33:05 +0800

    Create ClustrPairs.pl

---
+++ Shell/ClustrPairs.pl
@@ -0,0 +1,66 @@
+# Author: Prakki Sai Rama Sridatta
+
+# Date: January 28, 2020
+
+# A perl script to find the connected components from the list of pairs
+
+# Usage: perl ClustrPairs.pl pairs_tab_delim.list
+
+=a
+% cat pairs.list
+
+Jan<TABSPACE>Feb
+Jan<TABSPACE>Mar
+Jan<TABSPACE>Apr
+Jun<TABSPACE>Dec
+Jul<TABSPACE>Aug
+=cut
+
+
+#!/usr/bin/perl
+
+use Graph::Undirected;
+use File::Slurp;
+use List::MoreUtils qw(uniq);
+
+$g = Graph::Undirected->new;
+
+#use strict; 
+#use warnings;
+
+$filename="$ARGV[0]";
+
+open FH,"$filename";
+
+@set_array = map { chomp; [split /\t/] } <FH>;
+
+	foreach $tmp(@set_array)
+	{
+	#print "$tmp";
+	chomp($tmp);
+	($v1,$v2)=(@{$tmp}[0],@{$tmp}[1]);
+	#($v1,$v2)=split(" ",@{$tmp}[1],);
+	#print "V1 is $v1, V2 is $v2\n";
+	push(@temp,"$v1","$v2");
+	}
+
+@key_array = uniq(@temp); #Unique elements
+
+=aprint "@key_array\n";
+print "setarray-->@set_array\n";
+print "$set_array[0][0]---$set_array[0][1]\n";
+=cut
+
+$g->add_vertex( $_ ) for @key_array;
+$g->add_edge( $_->[0], $_->[1] ) for @set_array;
+
+$i=1;
+
+print "-----Cluster:".$i++."-------\n$_\n" for 
+    sort { length($b) <=> length($a) || $a cmp $b }
+    map {join( "\n",  @$_)}  
+    $g->connected_components;
+	
+	
+
+
