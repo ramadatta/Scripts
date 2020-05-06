08a32eecb1c90febd280fccc1e9666a25a7b6be5
Prakki Rama <ramadatta.88@gmail.com>
2020-05-06 17:39:22 +0800

    Create mob_typer_OnEachContig.sh

---
+++ Shell/mob_typer_OnEachContig.sh
@@ -0,0 +1,33 @@
+for filename in $(ls *.fasta); do 
+
+echo $filename; #Contains fasta filename
+
+mkdir "$filename"_Mob;
+
+	cd "$filename"_Mob;
+
+	while read line
+		do
+		    if [[ ${line:0:1} == '>' ]]
+		    then
+			outfile=${line#>}.fa
+			echo $line > $outfile
+		    else
+			echo $line >> $outfile
+		    fi
+		done < ../$filename;
+
+	for contigs in $(ls *.fa); 
+	
+		do
+		echo "##########------Working on $filename and $contigs-----------------############"
+		mob_typer --infile $contigs --outdir "$contigs"_out_dir;
+		done
+
+	cat */*report* >"$filename".Mob_report.txt
+
+	awk '!unique[$1$2$3$4]++' "$filename".Mob_report.txt > tmp && mv tmp "$filename".Mob_report.txt
+
+	cd ..;
+
+done
