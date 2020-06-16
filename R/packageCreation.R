cd0172993ed6816727a1adc6ee67fede4e2e6696
Prakki Rama <ramadatta.88@gmail.com>
2020-06-16 11:56:10 +0800

    Update packageCreation.R

--- R/packageCreation.R
+++ R/packageCreation.R
@@ -4,29 +4,61 @@ getwd()
 
 library(devtools)
 library(roxygen2)
-devtools::create("CPgeneProfiler")
-#Add the R script which you want to make it a package in the R directory created
+setwd("~/Desktop/CPE/")
+devtools::create("CPgeneProfiler",open = FALSE)
+  #Add the R script which you want to make it a package in the R directory created
 # Added imports to the Documentation file
 # Added documentation
-setwd("CPgeneProfiler/")
+getwd()
+
+setwd("~/Desktop/CPE/CPgeneProfiler/")
 #use_data(CPgeneProfiler)
 devtools::document() # to document if you have functions
 #devtools::load_all()
 setwd("..")
-install("CPgeneProfiler")
+install("CPgeneProfiler",dependencies = FALSE)
+?CPgeneProfiler
+??CPgeneProfiler
+.rs.restartR()
+library(CPgeneProfiler)
 
+# Install from Github
+devtools::install_github("ramadatta/CPgeneProfiler")
+CPgeneProfiler("/home/datta/Desktop/CPE/R_package/JOSS/Team_Comments/NPAssemblies","/home/datta/Desktop/CPE/R_package/JOSS/Team_Comments")
 
-
-
+  getAnywhere(CPgeneProfiler)
+remove.packages("CPgeneProfiler")
 
 #uploading
 # echo "# CPgeneProfiler" >> README.md
 # git init
 # git add README.md
+# git commit -m "Second commit"
 # git config --global user.email "ramadatta.88@gmail.com"
 # git remote add origin https://github.com/ramadatta/CPgeneProfiler.git # this did not work
+# git remote set-url origin https://github.com/ramadatta/CPgeneProfiler #this works too
 # git remote add origin https://github.com/ramadatta/CPgeneProfiler # this worked
+# $ git remote set-url origin git@github.com:ramadatta/CPgeneProfiler.git
+
 # git push -u origin master
 
+####This simply worked####
+# git init
+# git add .
+# git commit -m "Create first github package"
+# git remote add origin https://github.com/ramadatta/CPgeneProfiler # this worked
+# git remote set-url origin https://github.com/ramadatta/CPgeneProfiler #this works too
+# git push -f origin master # Force worked for updating 
+# git push origin master
+# ####
+
+https://stackoverflow.com/questions/17291995/push-existing-project-into-github
 
 
+# Adding to existing project
+# git add .
+# git commit -m "Create first github package"
+# git pull origin master
+# git push origin master
+
+#To push without conflicts both github and local should have same content.
